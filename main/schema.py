572fc040a9b26f9ab167a39c144fa2cec027d12b
Mazen Imara <mazen.imara@gmail.com>
2019-10-20 16:49:30 +0200

    use pk

--- main/schema.py
+++ main/schema.py
@@ -1,26 +1,60 @@
-from course.models import Course
-from status.models import Status
-from graphene import ObjectType, Node, Schema
+import graphene
+from graphene import ObjectType, Node, Schema, Int
 from graphene_django.fields import DjangoConnectionField
 from graphene_django.types import DjangoObjectType
 
-class CourseNode(DjangoObjectType):
+from django.contrib.auth.models import User
+from course.models import Course
+from status.models import Status
 
+################### Nodes #####################
+class UserNode(DjangoObjectType):
+    class Meta:
+        model = User
+        interfaces = (Node,)
+    pk = Int()
+    def resolve_pk(self, info):
+        return self.pk
+
+
+class CourseNode(DjangoObjectType):
     class Meta:
         model = Course
-        interfaces = (Node, )
+        interfaces = (Node,)
+    pk = Int()
+    def resolve_pk(self, info):
+        return self.pk
 
 class StatusNode(DjangoObjectType):
-
     class Meta:
         model = Status
-        interfaces = (Node, )
+        interfaces = (Node,)
+    pk = Int()
+    def resolve_pk(self, info):
+        return self.pk
 
+
+
+
+
+################### Query #####################
 class Query(ObjectType):
-    course = Node.Field(CourseNode)
-    all_courses = DjangoConnectionField(CourseNode)
+    users = DjangoConnectionField(UserNode)
+    user = graphene.Field(UserNode, pk=Int())
+    def resolve_user(self, info, **kwargs):
+        pk = kwargs.get('pk')
+        return User.objects.get(pk=pk)
 
-    status = Node.Field(StatusNode)
-    all_statuses = DjangoConnectionField(StatusNode)
+    courses = DjangoConnectionField(CourseNode)
+    course = graphene.Field(CourseNode, pk=Int())
+    def resolve_course(self, info, **kwargs):
+        pk = kwargs.get('pk')
+        return Course.objects.get(pk=pk)
+
+    statuses = DjangoConnectionField(StatusNode)
+    status = graphene.Field(StatusNode, pk=Int())
+    def resolve_status(self, info, **kwargs):
+        pk = kwargs.get('pk')
+        return Status.objects.get(pk=pk)
 
 schema = Schema(query=Query)
