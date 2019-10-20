782bc0155ffb44ca8065866bdb476bd3ee732a24
Mazen Imara <mazen.imara@gmail.com>
2019-10-20 02:07:04 +0200

    first commit

---
+++ main/settings.py
@@ -0,0 +1,13 @@
+from .commonSettings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3305',
+    }
+}
+
