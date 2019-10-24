06d0278d25e160dacd1faf864296b1c8753d1c92
Mazen Imara <mazen.imara@gmail.com>
2019-10-24 02:39:10 +0200

    done with login and authentication

--- main/schema.py
+++ main/schema.py
@@ -3,20 +3,32 @@ from graphene import ObjectType, Node, Schema, Int
 from graphene_django.fields import DjangoConnectionField
 from graphene_django.types import DjangoObjectType
 import graphql_jwt
+from graphql_jwt.decorators import login_required
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from course.models import Course
 from status.models import Status
 
 
 ################### Nodes #####################
+
+class PermissionNode(DjangoObjectType):
+    class Meta:
+        model = Permission
+    pk = Int()
+
+class GroupNode(DjangoObjectType):
+    class Meta:
+        model = Group
+    pk = Int()
+    permissins = PermissionNode
+
 class UserNode(DjangoObjectType):
     class Meta:
         model = User
         interfaces = (Node,)
-
     pk = Int()
-
+    groups = GroupNode()
     def resolve_pk(self, info):
         return self.pk
 
@@ -25,9 +37,7 @@ class CourseNode(DjangoObjectType):
     class Meta:
         model = Course
         interfaces = (Node,)
-
     pk = Int()
-
     def resolve_pk(self, info):
         return self.pk
 
@@ -36,25 +46,37 @@ class StatusNode(DjangoObjectType):
     class Meta:
         model = Status
         interfaces = (Node,)
-
     pk = Int()
-
     def resolve_pk(self, info):
         return self.pk
 
 
 ################### Query #####################
 class Query(ObjectType):
+    ### Courses ###
     users = DjangoConnectionField(UserNode)
-    user = graphene.Field(UserNode, pk=Int())
+    @login_required
+    def resolve_courses(self, info, **kwargs):
+        return self
 
+    user = graphene.Field(UserNode, pk=Int())
+    @login_required
     def resolve_user(self, info, **kwargs):
         pk = kwargs.get('pk')
         return User.objects.get(pk=pk)
 
-    courses = DjangoConnectionField(CourseNode)
-    course = graphene.Field(CourseNode, pk=Int())
+    logedUser = graphene.Field(UserNode)
+    @login_required
+    def resolve_logedUser(self, info, **kwargs):
+        return info.context.user
 
+    ### Courses ###
+    courses = DjangoConnectionField(CourseNode)
+    @login_required
+    def resolve_courses(self, info, **kwargs):
+        return self
+
+    course = graphene.Field(CourseNode, pk=Int())
     def resolve_course(self, info, **kwargs):
         pk = kwargs.get('pk')
         return Course.objects.get(pk=pk)
